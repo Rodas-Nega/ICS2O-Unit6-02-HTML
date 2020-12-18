2be7553095512cf4a39dd97ee21eee4f7449eb93
RodasNega <rodas.nega@ocsbstudent.ca>
2020-12-18 22:43:36 +0000

    Oops, accidentally deleted indents for if statement while correcting.

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function displayInteger () {
   const number = document.getElementById('whole-number').value
   // the modulus is to make the program to detect decimals.
   if (number % 1) {
-  alert('Eh, maybe stray away from decimals.')
+    alert('Eh, maybe stray away from decimals.')
   // the <= symbol is used to detect negatives.
   } else if (number <= 0) {
     alert('Your number is a negative.')
