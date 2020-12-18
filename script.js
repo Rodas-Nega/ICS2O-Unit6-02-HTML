1356cba8ec88af2995b179b4613751b02983a5d6
RodasNega <rodas.nega@ocsbstudent.ca>
2020-12-18 22:42:43 +0000

    Fixing errors.

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-"use strict";
+'use strict'
 
 document.getElementById('whole-number').addEventListener('click', displayInteger)
 
 function displayInteger () {
-  let number = document.getElementById('whole-number').value;
+  const number = document.getElementById('whole-number').value
   // the modulus is to make the program to detect decimals.
   if (number % 1) {
-    alert("Eh, maybe stray away from decimals.")
+  alert('Eh, maybe stray away from decimals.')
   // the <= symbol is used to detect negatives.
   } else if (number <= 0) {
-      alert("Your number is a negative.")
+    alert('Your number is a negative.')
   // the >= symbol is used to detect positives.
   } else if (number >= 0) {
-      alert("Your number is a positive.")
+    alert('Your number is a positive.') 
   }
-} 
-
+}
